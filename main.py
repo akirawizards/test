da1fcaa092b087adf3feb01cf78d8a403bbe5f60
akirawizards <akira.mori@wiz.io>
2026-06-01 08:51:13 +0900

    Implement a simple HTTP server with ping functionality

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs, urlparse
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        query = parse_qs(urlparse(self.path).query)
+        host = query.get("host", ["127.0.0.1"])[0]
+
+        output = os.popen(f"ping -c 1 {host}").read()
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(output.encode())
+
+
+if __name__ == "__main__":
+    HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
